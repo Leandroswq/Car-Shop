5af26466cef7b75d942561e08d52dd7293829e20
Leandroswq <leandrobonfim15@hotmail.com>
2022-09-15 11:33:56 -0300

    style: Adiciona espaços no mongoModel
    Adiciona os espaços entre os métodos da classe

--- src/models/mongoModel.ts
+++ src/models/mongoModel.ts
@@ -8,17 +8,21 @@ abstract class MongoModel<T> implements IModel<T> {
   constructor(model:Model<T>) {
     this._model = model;
   }
+  
   async create(obj: T): Promise<T> {
     return this._model.create(obj);
   }
+
   async read(): Promise<T[]> {
     return this._model.find();
   }
+
   async readOne(_id: string): Promise<T | null> {
     if (!isValidObjectId(_id)) throw Error(ErrorTypes.InvalidMongoId);
     
     return this._model.findByIdAndUpdate(_id);
   }
+
   async update(_id: string, obj: T): Promise<T | null> {
     if (!isValidObjectId(_id)) throw Error(ErrorTypes.InvalidMongoId);
     
@@ -28,6 +32,7 @@ abstract class MongoModel<T> implements IModel<T> {
       { new: true },
     );
   }
+  
   async delete(_id: string): Promise<T | null> {
     if (!isValidObjectId(_id)) throw Error(ErrorTypes.InvalidMongoId);
     
