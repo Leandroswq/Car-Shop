cac48f5ac388a50907462ad633f08a2c12f158bf
Leandroswq <leandrobonfim15@hotmail.com>
2022-09-16 15:29:38 -0300

    feat: Cria a interface do CrudController

---
+++ src/interfaces/ICrudeController.ts
@@ -0,0 +1,9 @@
+import { Request, Response } from 'express';
+
+export interface ICRudController<T>{
+  create(req: Request, res: Response): Promise<Response<T>>,
+  read(req: Request, res: Response): Promise<Response<T[]>>,
+  readOne(req: Request, res: Response): Promise<Response<T>>,
+  update(req: Request, res: Response): Promise<Response<T>>,
+  delete(req: Request, res: Response): Promise<Response>,
+}
