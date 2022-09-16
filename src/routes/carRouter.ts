c9d547768a3dad9aa01abee927be28947c6ac24d
Leandroswq <leandrobonfim15@hotmail.com>
2022-09-16 17:50:23 -0300

    feat: Cria a rota que deleta um carro no DB pelo id

--- src/routes/carRouter.ts
+++ src/routes/carRouter.ts
@@ -12,5 +12,6 @@ const carController = new CarController(carService);
 carRouter.post('/', (req, res) => carController.create(req, res));
 carRouter.get('/', (req, res) => carController.read(req, res));
 carRouter.get('/:id', (req, res) => carController.readOne(req, res));
+carRouter.delete('/:id', (req, res) => carController.delete(req, res));
 
 export default carRouter;
