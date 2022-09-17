6ebe467fcbd848d3a2499dddd1787ec4a4073d2e
Leandroswq <leandrobonfim15@hotmail.com>
2022-09-16 22:29:41 -0300

    feat: Adiciona a rota que busca uma moto pelo id

--- src/routes/motorcycleRouter.ts
+++ src/routes/motorcycleRouter.ts
@@ -11,5 +11,6 @@ const motorcycleController = new MotorcycleController(motorcycleService);
 
 motorcycleRouter.post('/', (req, res) => motorcycleController.create(req, res));
 motorcycleRouter.get('/', (req, res) => motorcycleController.read(req, res));
+motorcycleRouter.get('/:id', (req, res) => motorcycleController.readOne(req, res));
 
 export default motorcycleRouter;
