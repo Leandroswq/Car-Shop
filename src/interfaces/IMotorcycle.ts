665f7022f88ae0bf2f1839526dd221f691516277
Leandroswq <leandrobonfim15@hotmail.com>
2022-09-16 22:26:08 -0300

    fix: Atualiza o MotorcycleZodSchema
    Atualiza o valor mínimo para 1

--- src/interfaces/IMotorcycle.ts
+++ src/interfaces/IMotorcycle.ts
@@ -3,12 +3,12 @@ import { VehicleUpdateZodSchema, VehicleZodSchema } from './IVehicle';
 
 export const MotorcycleZodSchema = VehicleZodSchema.extend({
   category: z.enum(['Street', 'Custom', 'Trail']),
-  engineCapacity: z.number().int().min(0).max(2500),
+  engineCapacity: z.number().int().min(1).max(2500),
 });
 
 export const MotorcycleUpdateZodSchema = VehicleUpdateZodSchema.extend({
   category: z.enum(['Street', 'Custom', 'Trail']).optional(),
-  engineCapacity: z.number().int().min(0).max(2500)
+  engineCapacity: z.number().int().min(1).max(2500)
     .optional(),
 });
 
