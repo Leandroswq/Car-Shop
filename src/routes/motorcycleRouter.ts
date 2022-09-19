65f27361d2c699793c2fd2446fd71d8165f5d510
Leandroswq <leandrobonfim15@hotmail.com>
2022-09-19 06:48:24 -0300

    feat: Adiciona a rota que atualiza uma moto pelo id

--- src/routes/motorcycleRouter.ts
+++ src/routes/motorcycleRouter.ts
@@ -13,5 +13,6 @@ motorcycleRouter.post('/', (req, res) => motorcycleController.create(req, res));
 motorcycleRouter.get('/', (req, res) => motorcycleController.read(req, res));
 motorcycleRouter.get('/:id', (req, res) => motorcycleController.readOne(req, res));
 motorcycleRouter.delete('/:id', (req, res) => motorcycleController.delete(req, res));
+motorcycleRouter.put('/:id', (req, res) => motorcycleController.update(req, res));
 
 export default motorcycleRouter;
