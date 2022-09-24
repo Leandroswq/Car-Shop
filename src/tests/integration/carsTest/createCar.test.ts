4897231c687515d3a72d70c5e20b5d80b83990d7
Leandroswq <leandrobonfim15@hotmail.com>
2022-09-24 11:10:06 -0300

    fix<integração> Corrige o retorno dos mocks

--- src/tests/integration/carsTest/createCar.test.ts
+++ src/tests/integration/carsTest/createCar.test.ts
@@ -18,16 +18,16 @@ describe("Endpoint /cars do tipo post", () => {
   })
 
   it('É possível adicionar um carro se todos os parâmetros estiverem corretos', async () => {
-    const createStub = sinon.stub(Model, 'create').resolves([carMock.validCarWithId])
+    const createStub = sinon.stub(Model, 'create').resolves(carMock.validCarWithId)
     const response = await request(carMock.validCar)
 
     expect(response.status).to.equal(201)
-    expect(response.body).to.be.deep.equal([carMock.validCarWithId])
+    expect(response.body).to.be.deep.equal(carMock.validCarWithId)
     expect(createStub.calledOnce).to.be.true
   })
 
   it('Retorna um erro 400 caso o objeto esteja vazio', async () => {
-    const createStub = sinon.stub(Model, 'create').resolves([carMock.validCarWithId])
+    const createStub = sinon.stub(Model, 'create').resolves(carMock.validCarWithId)
     const response = await request({})
 
     expect(response.status).to.equal(400)
@@ -35,7 +35,7 @@ describe("Endpoint /cars do tipo post", () => {
   })
 
   it('Retorna um erro 400 ao tentar criar um carro com quantidade de assentos inferior a 2', async () => {
-    const createStub = sinon.stub(Model, 'create').resolves([carMock.validCarWithId])
+    const createStub = sinon.stub(Model, 'create').resolves(carMock.validCarWithId)
     const response = await request(carMock.carSeatsLtTwo)
 
     expect(response.status).to.equal(400)
@@ -43,7 +43,7 @@ describe("Endpoint /cars do tipo post", () => {
   })
 
   it('Retorna um erro 400 ao tentar criar um carro com quantidade de assentos superior a 7', async () => {
-    const createStub = sinon.stub(Model, 'create').resolves([carMock.validCarWithId])
+    const createStub = sinon.stub(Model, 'create').resolves(carMock.validCarWithId)
     const response = await request(carMock.carSeatsGtSeven)
 
     expect(response.status).to.equal(400)
@@ -51,7 +51,7 @@ describe("Endpoint /cars do tipo post", () => {
   })
 
   it('Retorna um erro 400 ao tentar criar um carro com quantidade de portas inferior a 2', async () => {
-    const createStub = sinon.stub(Model, 'create').resolves([carMock.validCarWithId])
+    const createStub = sinon.stub(Model, 'create').resolves(carMock.validCarWithId)
     const response = await request(carMock.carDoorsLtTwo)
 
     expect(response.status).to.equal(400)
@@ -59,7 +59,7 @@ describe("Endpoint /cars do tipo post", () => {
   })
 
   it('Retorna um erro 400 ao tentar criar um carro com quantidade de portas superior a 4', async () => {
-    const createStub = sinon.stub(Model, 'create').resolves([carMock.validCarWithId])
+    const createStub = sinon.stub(Model, 'create').resolves(carMock.validCarWithId)
     const response = await request(carMock.carDoorsGtFour)
 
     expect(response.status).to.equal(400)
@@ -67,7 +67,7 @@ describe("Endpoint /cars do tipo post", () => {
   })
 
   it('Retorna um erro 400 caso ao tentar criar um carro sem algum dos campos "model", "year", "color", "buyValue", "doorsQty e "seatsQty"', async () => {
-    const createStub = sinon.stub(Model, 'create').resolves([carMock.validCarWithId])
+    const createStub = sinon.stub(Model, 'create').resolves(carMock.validCarWithId)
     let response = await request(carMock.noModelCar)
     expect(response.status).to.equal(400)
 
