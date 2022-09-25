faed828b7a028f00f031506b02c339daad6eb085
Leandroswq <leandrobonfim15@hotmail.com>
2022-09-25 16:52:37 -0300

    feat: Adiciona rota da documentação

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,18 @@
 import express from 'express';
 import 'express-async-errors';
+import swaggerUI from 'swagger-ui-express';
+import YAML from 'yamljs';
 import carRouter from './routes/carRouter';
 import errorHandler from './middlewares/error';
 import motorcycleRouter from './routes/motorcycleRouter';
+
+const docs = YAML.load('src/swaggerDocs.yml');
 
 const app = express();
 app.use(express.json());
 
 // Rotas
+app.use('/docs', swaggerUI.serve, swaggerUI.setup(docs));
 app.use('/cars', carRouter);
 app.use('/motorcycles', motorcycleRouter);
 
