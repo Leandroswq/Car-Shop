c2a35d01d5185ea6b0e65b320255033b47615965
Leandroswq <leandrobonfim15@hotmail.com>
2022-09-25 17:06:36 -0300

    feat: Configura o cors

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import 'express-async-errors';
 import swaggerUI from 'swagger-ui-express';
 import YAML from 'yamljs';
+import cors from 'cors';
 import carRouter from './routes/carRouter';
 import errorHandler from './middlewares/error';
 import motorcycleRouter from './routes/motorcycleRouter';
@@ -10,6 +11,7 @@ const docs = YAML.load('src/swaggerDocs.yml');
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 // Rotas
 app.use('/docs', swaggerUI.serve, swaggerUI.setup(docs));
